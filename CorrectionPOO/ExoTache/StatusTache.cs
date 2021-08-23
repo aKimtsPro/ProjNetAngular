24feb5157807b2c49dfce3884b6d2f8566d98c5b
aKimts <a.kimtsaris@bstorm.be>
2021-08-23 11:15:54 +0200

    correction corrigé

---
+++ CorrectionPOO/ExoTache/StatusTache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionPOO.ExoTache
+{
+    public enum StatusTache
+    {
+        A_FAIRE,
+        EN_PREPARATION,
+        EN_COURS,
+        TERMINE
+    }
+}
---
+++ CorrectionPOO/ExoTache/Tache.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionPOO.ExoTache
+{
+
+    public class Tache
+    {
+		private string _titre = "inconnu";
+		private StatusTache _status;
+
+		public string Titre
+		{
+			get { return _titre; }
+			private set {
+				if( value.Length <= 20 )
+				_titre = value; 
+			}
+		}
+		public string Description { get; set; }
+		public StatusTache Status
+		{
+			get { return _status; }
+			set { 
+				if( _status < value )
+					_status = value;
+			}
+		}
+
+		public Tache(string titre) : this(titre, StatusTache.A_FAIRE)
+		{}
+		public Tache(string titre, StatusTache status)
+		{
+			Titre = titre;
+			Status = status;
+		}
+		public Tache(string titre, string desc, StatusTache status)
+		{
+			Titre = titre;
+			Description = desc;
+			Status = status;
+		}
+
+		public bool Avancer()
+		{
+			if (Status == StatusTache.TERMINE)
+				return true;
+
+			Status++;
+
+			return Status == StatusTache.TERMINE;
+		}
+
+
+	}
+}
